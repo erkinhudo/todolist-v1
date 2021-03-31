735d138e59728f1d425f7e8c12341ad614bbde7e
Eric Hudo <erkinhudo@gmail.com>
2021-03-31 01:33:59 -0400

    styles and functions  added

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [ "Buy Food","Cook Food","Eat Food"];
+let items = [ "Buy Food","Cook Food","Eat Food"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get ("/",function(req,res){
    
-  var today =  new Date();
+  let today =  new Date();
   
-  var options = {
+  let options = {
     weekday : "long",
     day : "numeric",
-    month : "long"
+    month : "long",
+    year : "numeric"
   };
   
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
 
   res.render("list",{kindOfDay: day , newItemList: items });
