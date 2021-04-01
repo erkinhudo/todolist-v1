98cd9325b32fe1a04f0528f99df5d1f053ebd2c7
Eric Hudo <erkinhudo@gmail.com>
2021-04-01 01:00:58 -0400

    work page is created

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = [ "Buy Food","Cook Food","Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -24,15 +25,38 @@ app.get ("/",function(req,res){
   let day = today.toLocaleDateString("en-US", options);
 
 
-  res.render("list",{kindOfDay: day , newItemList: items });
+  res.render("list",{listTitle: day , newItemList: items });
 });
 
 
 app.post("/",function(req,res){
-  var item = req.body.newItem;
+ 
+  let item = req.body.newItem;
+if(req.body.list === "Work"){
+   workItems.push(item);
+   res.redirect("/work")
+}
+else{
   items.push(item);
   res.redirect("/")
+}
+  
+ 
 });
+
+app.get ("/work", function(req,res){
+  res.render("list", {listTitle: "Work List ",newItemList: workItems});
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
+})
+
+app.post("/work",function(req,res){
+  let item =req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
